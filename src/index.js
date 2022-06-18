b0a8f3d1ffe5a0fec05b5195c8b605561005adf1
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-17 19:46:51 -0500

    feeling reducer

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Redux Stuff
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Reducers
+const feelingReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'SET_FEELING':
+            return action.payload
+    }
+    return state;
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
